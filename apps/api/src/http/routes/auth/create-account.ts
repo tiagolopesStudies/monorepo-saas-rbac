a2e5360e93f572fb07a9ed6f0d4da48a64f108ba
Tiago Lopes <tiagoltavares2002@gmail.com>
2026-06-22 21:07:06 -0300

    feat: implementando cadastro do usuário e inserção no banco de dados

--- apps/api/src/http/routes/auth/create-account.ts
+++ apps/api/src/http/routes/auth/create-account.ts
@@ -1,6 +1,8 @@
+import bcrypt from 'bcryptjs'
 import type { FastifyInstance } from 'fastify'
 import type { ZodTypeProvider } from 'fastify-type-provider-zod'
 import z from 'zod'
+import { prisma } from '@/lib/prisma'
 
 export async function createAccount(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().post(
@@ -14,7 +16,27 @@ export async function createAccount(app: FastifyInstance) {
         }),
       },
     },
-    (_, reply) => {
+    async (request, reply) => {
+      const { name, email, password } = request.body
+
+      const userWithSameEmail = await prisma.user.findFirst({
+        where: { email },
+      })
+
+      if (userWithSameEmail !== null) {
+        return reply.status(409).send({
+          error: 'A user with that same email address already exists',
+        })
+      }
+
+      await prisma.user.create({
+        data: {
+          name,
+          email,
+          passwordHash: await bcrypt.hash(password, 6),
+        },
+      })
+
       return reply.status(201).send({
         message: 'User created!',
       })
